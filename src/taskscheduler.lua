38bd506700269ab1a2a2034228870934b2ea2df3
PhantomShift <83253309+PhantomShift@users.noreply.github.com>
2021-05-01 02:10:14 -0400

    Changed to make use of coroutines

--- src/taskscheduler.lua
+++ src/taskscheduler.lua
@@ -1,20 +1,52 @@
--- Task scheduler that manages all COROUTINES that rely on some sort of time delay
-local taskscheduler = {__tasks = {}}
-function taskscheduler.addTask(Coroutine, Lifetime)
-    taskscheduler.__tasks[{Coroutine = Coroutine, Lifetime = Lifetime}] = 0
-end
-function taskscheduler.update(elapsedTime)
-    for c, t in pairs(taskscheduler.__tasks) do
-        t = t + elapsedTime
-        --print(t)
-        if t > c.Lifetime then
-            c.Coroutine()
-            --print(c.Coroutine, c.Lifetime)
-            taskscheduler.__tasks[c] = nil
-        else
-            taskscheduler.__tasks[c] = t
+local BindableEvent = require "BindableEvent"
+local coroutine = require "coroutine"
+local tick = os.time
+
+local frametime = 1/60
+local __await = {}
+local __time = tick()
+local taskscheduler = {
+    Stepped = BindableEvent.new()
+}
+
+local main_thread = coroutine.create(function()
+    while true do
+        local currentTime = tick()
+        local dt = currentTime - __time
+        if dt > frametime then
+            taskscheduler.Stepped:Fire(dt)
         end
+        __time = currentTime
+        for c, delay in pairs(__await) do
+            if __time > delay then
+                local s, e = coroutine.resume(c)
+                if not s then error(e) end
+                __await[c] = nil
+            end
+        end
+        coroutine.yield()
     end
+end)
+
+function taskscheduler.update(elapsedTime)
+    local success, err = coroutine.resume(main_thread)
+    if not success then
+        error(table.concat({"Thread error from", tostring(main_thread), ":", err}))
+    end
+end
+
+-- Note that a delay of 0 is practically just spawning a thread; if simply spawning a new thread is desired, consider using coroutine.wrap
+function taskscheduler.delay(delay, func, ...)
+    local delay = delay or 0
+    if delay < 0 then delay = 0 end 
+    local t = {...} or nil
+    local c
+    if t then
+        c = coroutine.create(function() func(unpack(t)) end)
+    else
+        c = coroutine.create(func)
+    end
+    __await[c] = delay + tick()
 end
 
 return taskscheduler
